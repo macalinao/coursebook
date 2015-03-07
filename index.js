1761cae805ecdd5cf9e6e8d06cde8618fe8f10cd
Ian Macalinao <me@ian.pw>
2015-03-07 13:12:04 -0600

    Add schedule, loc, and fill

--- index.js
+++ index.js
@@ -10,19 +10,28 @@ module.exports = {
       var ret = [];
       $('.section-list table tbody tr').each(function() {
         var $r = $(this).children('td');
-        var c0 = $($r[0]);
-        var c1 = $($r[1]);
 
         // Parse
+        var c0 = $($r[0]);
         var term = c0.clone().children().remove().end().text();
         var status = $(c0.children('span')[0]).text();
+
+        var c1 = $($r[1]);
         var section = $(c1.children('a')[0]).text();
         var num = c1.clone().children().remove().end().text();
+
         var title = $($r[2]).text();
+
         var instructors = [];
         $($r[3]).children('a').each(function() {
           instructors.push($(this).text().trim());
         });
+
+        var c4 = $($r[4]);
+        var schedule = c4.clone().children().remove().end().text();
+        var loc = $(c4.children('a')[0]).text();
+
+        var fill = $($($r[5]).children('div')[0]).attr('title').split(' ')[0];
 
         ret.push({
           term: term,
@@ -30,7 +39,10 @@ module.exports = {
           section: section,
           num: num,
           title: title,
-          instructors: instructors
+          instructors: instructors,
+          schedule: schedule,
+          loc: loc,
+          fill: fill
         });
       });
       return ret;
