5726d2da904b1b315df41969944dea728077b9dd
Ian Macalinao <me@ian.pw>
2015-03-07 13:05:30 -0600

    Add title

--- index.js
+++ index.js
@@ -18,12 +18,14 @@ module.exports = {
         var status = $(c0.children('span')[0]).text();
         var section = $(c1.children('a')[0]).text();
         var num = c1.clone().children().remove().end().text();
+        var title = $($r[2]).text();
 
         ret.push({
           term: term,
           status: status,
           section: section,
-          num: num
+          num: num,
+          title: title
         });
       });
       return ret;
